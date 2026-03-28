3227df71bbe1d63521cd03e4237bfe05b99eb679
Alessio Piroli <alessiopiroli@Alessios-MacBook-Air.local>
2026-03-28 15:33:29 +0100

    Added test for entire architecture

--- vit/test/test_model.py
+++ vit/test/test_model.py
@@ -1,30 +1,37 @@
 import pytest
 import torch
 from easydict import EasyDict as edict
-from vit.model.vit_model import ImagePatcher, HeadAttention, ViTEncoder
+from vit.model.vit_model import ImagePatcher, AttentionHead, ViTEncoder, ViT
 from vit.utils.misc import load_config
 
 @pytest.fixture
-def config():
+def cfg():
     return load_config("vit/config/vit_config.yml")
 
 @pytest.mark.parametrize("bs, n_ch, h, w", [(4, 3, 224, 224)])
-def test_img_patcher(config, bs, n_ch, h, w):
-    img_size = config.IMG.img_size
-    patch_size = config.IMG.patch_size
+def test_img_patcher(cfg, bs, n_ch, h, w):
+    img_size = cfg.IMG.img_size
+    patch_size = cfg.IMG.patch_size
     n_patches = (img_size // patch_size) ** 2
     x = torch.randn(bs, n_ch, h, w)
-    out = ImagePatcher(config)(x)
-    assert out.shape == (bs, n_patches + 1, config.MODEL.IMG_PATCHER.latent_dim)
+    out = ImagePatcher(cfg)(x)
+    assert out.shape == (bs, n_patches + 1, cfg.MODEL.IMG_PATCHER.latent_dim)
 
 @pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 197, 512)])
-def test_head_attn(config, bs, n_vec, l_dim):
+def test_head_attn(cfg, bs, n_vec, l_dim):
     x = torch.randn(bs, n_vec, l_dim)
-    out = HeadAttention(config)(x)
-    assert out.shape == (bs, n_vec, l_dim // config.MODEL.ViT_ENCODER.n_heads)
+    out = AttentionHead(cfg)(x)
+    assert out.shape == (bs, n_vec, l_dim // cfg.MODEL.ViT_ENCODER.n_heads)
 
 @pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 197, 512)])
-def test_vit_enc(config, bs, n_vec, l_dim):
+def test_vit_enc(cfg, bs, n_vec, l_dim):
     x = torch.randn(bs, n_vec, l_dim)
-    out = ViTEncoder(config)(x)
+    out = ViTEncoder(cfg)(x)
     assert out.shape == x.shape
+
+@pytest.mark.parametrize("bs, n_ch, h, w", [(4, 3, 224, 224)])
+def test_vit(cfg, bs, n_ch, h, w):
+    n_cls = cfg.MODEL.HEAD.n_classes
+    x = torch.randn(bs, n_ch, h, w)
+    out = ViT(cfg)(x)
+    assert out.shape == (bs, n_cls)
