d3e292d5a92e8c32528bf87468b9210dd14fb07d
Alessio Piroli <alessiopiroli@Alessios-MacBook-Air.local>
2026-03-28 15:03:14 +0100

    Head attention, encoder architectures, definition and tests

--- vit/model/vit_model.py
+++ vit/model/vit_model.py
@@ -8,7 +8,7 @@ class ImagePatcher(nn.Module):
         self.patch_size = self.cfg.IMG.patch_size
         self.n_patches = (self.cfg.IMG.img_size // self.patch_size) ** 2
         self.lin_in = (self.patch_size ** 2) * 3
-        self.lin_out = self.cfg.MODEL.latent_size
+        self.lin_out = self.cfg.MODEL.IMG_PATCHER.latent_dim
 
         self.unfold = nn.Unfold(kernel_size=self.cfg.IMG.patch_size, stride=self.cfg.IMG.patch_size)
         self.linear_proj = nn.Linear(self.lin_in, self.lin_out)
@@ -21,4 +21,52 @@ class ImagePatcher(nn.Module):
         class_token = self.class_token.expand(x.shape[0], -1, -1)
         x = torch.cat([class_token, x], dim=-2)
         x = x + self.pos_embedding
+        return x
+    
+
+class HeadAttention(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.latent_dim = self.cfg.MODEL.IMG_PATCHER.latent_dim
+        self.out_dim = self.latent_dim // self.cfg.MODEL.ViT_ENCODER.n_heads
+
+        self.linear_query = nn.Linear(self.latent_dim, self.out_dim)
+        self.linear_key = nn.Linear(self.latent_dim, self.out_dim)
+        self.linear_value = nn.Linear(self.latent_dim, self.out_dim)
+    
+    def forward(self, x):
+        Q = self.linear_query(x)
+        K = self.linear_key(x)
+        V = self.linear_value(x)
+
+        attn = (Q @ torch.transpose(K, dim0=-2, dim1=-1)) / (self.out_dim ** 0.5)
+        attn = torch.softmax(attn, dim=-1)
+        attn = attn @ V
+        return attn
+    
+
+class ViTEncoder(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.n_heads = self.cfg.MODEL.ViT_ENCODER.n_heads
+        self.latent_dim = self.cfg.MODEL.IMG_PATCHER.latent_dim
+
+        self.layer_norm = nn.LayerNorm(self.latent_dim)
+        self.attn_heads = nn.ModuleList([HeadAttention(self.cfg) for _ in range(self.n_heads)])
+        self.attn_proj = nn.Linear(self.latent_dim, self.latent_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(self.latent_dim, self.latent_dim*4),
+            nn.GELU(),
+            nn.Linear(4*self.latent_dim, self.latent_dim)
+        )
+
+    def forward(self, x):
+        x_norm = self.layer_norm(x)
+        attn_out = torch.cat([head(x_norm) for head in self.attn_heads], dim=-1)
+        x = self.attn_proj(attn_out) + x
+        x_norm = self.layer_norm(x)
+        x = self.mlp(x_norm) + x
+        
         return x
--- vit/test/test_model.py
+++ vit/test/test_model.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 from easydict import EasyDict as edict
-from vit.model.vit_model import ImagePatcher
+from vit.model.vit_model import ImagePatcher, HeadAttention, ViTEncoder
 from vit.utils.misc import load_config
 
 @pytest.fixture
@@ -15,4 +15,16 @@ def test_img_patcher(config, bs, n_ch, h, w):
     n_patches = (img_size // patch_size) ** 2
     x = torch.randn(bs, n_ch, h, w)
     out = ImagePatcher(config)(x)
-    assert out.shape == (bs, n_patches + 1, config.MODEL.latent_size)
+    assert out.shape == (bs, n_patches + 1, config.MODEL.IMG_PATCHER.latent_dim)
+
+@pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 197, 512)])
+def test_head_attn(config, bs, n_vec, l_dim):
+    x = torch.randn(bs, n_vec, l_dim)
+    out = HeadAttention(config)(x)
+    assert out.shape == (bs, n_vec, l_dim // config.MODEL.ViT_ENCODER.n_heads)
+
+@pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 197, 512)])
+def test_vit_enc(config, bs, n_vec, l_dim):
+    x = torch.randn(bs, n_vec, l_dim)
+    out = ViTEncoder(config)(x)
+    assert out.shape == x.shape
