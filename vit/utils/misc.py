9bfd5daaa769f87a4843fd838e3116e59f34ad03
Alessio Piroli <alessiopiroli@Alessios-MacBook-Air.local>
2026-03-28 07:36:23 +0100

    Added function to load configuration

---
+++ vit/utils/misc.py
@@ -0,0 +1,7 @@
+from easydict import EasyDict as edict
+import yaml
+
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    return edict(config)
