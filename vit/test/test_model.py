7a97d1e7a46a8a669f6f05ed260f11cd10747c81
Alessio Piroli <alessiopiroli@Alessios-MacBook-Air.local>
2026-03-31 08:40:28 +0200

    Updated test to match logic

--- vit/test/test_model.py
+++ vit/test/test_model.py
@@ -10,7 +10,7 @@ def cfg():
     return load_config("vit/config/vit_config.yml")
 
 
-@pytest.mark.parametrize("bs, n_ch, h, w", [(4, 3, 224, 224)])
+@pytest.mark.parametrize("bs, n_ch, h, w", [(4, 3, 64, 64)])
 def test_img_patcher(cfg, bs, n_ch, h, w):
     img_size = cfg.IMG.img_size
     patch_size = cfg.IMG.patch_size
@@ -20,21 +20,21 @@ def test_img_patcher(cfg, bs, n_ch, h, w):
     assert out.shape == (bs, n_patches + 1, cfg.MODEL.IMG_PATCHER.latent_dim)
 
 
-@pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 197, 512)])
+@pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 65, 256)])
 def test_head_attn(cfg, bs, n_vec, l_dim):
     x = torch.randn(bs, n_vec, l_dim)
     out = AttentionHead(cfg)(x)
     assert out.shape == (bs, n_vec, l_dim // cfg.MODEL.ViT_ENCODER.n_heads)
 
 
-@pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 197, 512)])
+@pytest.mark.parametrize("bs, n_vec, l_dim", [(4, 65, 256)])
 def test_vit_enc(cfg, bs, n_vec, l_dim):
     x = torch.randn(bs, n_vec, l_dim)
     out = ViTEncoder(cfg)(x)
     assert out.shape == x.shape
 
 
-@pytest.mark.parametrize("bs, n_ch, h, w", [(4, 3, 224, 224)])
+@pytest.mark.parametrize("bs, n_ch, h, w", [(4, 3, 64, 64)])
 def test_vit(cfg, bs, n_ch, h, w):
     n_cls = cfg.MODEL.HEAD.n_classes
     x = torch.randn(bs, n_ch, h, w)
